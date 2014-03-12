635039190e63c9c6d760a46167decea6443d9baa
Wilhelm Matilainen <wilhelm.matilainen@gmail.com>
2014-03-12 22:14:14 +0200

    Fix categories

--- index.js
+++ index.js
@@ -4,61 +4,61 @@ var async = require('async');
 var magnet = require('magnet-uri');
 
 var categories = {
-	"audio": {
-		"music": "101",
-		"audiobooks": "102",
-		"soundclips": "103",
-		"flac": "104",
-		"other": "199"
+	audio: {
+		music: '101',
+		audiobooks: '102',
+		soundclips: '103',
+		flac: '104',
+		other: '199'
 	},
-	"video": {
-		"movies": "201",
-		"moviesdvdr": "202",
-		"musicvideos": "203",
-		"movieclips": "204",
-		"tvshows": "205",
-		"handheld": "206",
-		"hdmovies": "207",
-		"hdtvshows": "208",
-		"3d": "209",
-		"other": "299"
+	video: {
+		movies: '201',
+		moviesdvdr: '202',
+		musicvideos: '203',
+		movieclips: '204',
+		tvshows: '205',
+		handheld: '206',
+		hdmovies: '207',
+		hdtvshows: '208',
+		movies3d: '209',
+		other: '299'
 	},
-	"application": {
-		"windows": "301",
-		"mac": "302",
-		"unix": "303",
-		"handheld": "304",
-		"ios": "305",
-		"android": "306",
-		"other os": "399"
+	application: {
+		windows: '301',
+		mac: '302',
+		unix: '303',
+		handheld: '304',
+		ios: '305',
+		android: '306',
+		other os: '399'
 	},
-	"games": {
-		"pc": "401",
-		"mac": "402",
-		"psx": "403",
-		"xbox360": "404",
-		"wii": "405",
-		"handheld": "406",
-		"ios": "407",
-		"android": "408",
-		"other": "499"
+	games: {
+		pc: '401',
+		mac: '402',
+		psx: '403',
+		xbox360: '404',
+		wii: '405',
+		handheld: '406',
+		ios: '407',
+		android: '408',
+		other: '499'
 	},
-	"porn": {
-		"movies": "501",
-		"moviesdvdr": "502",
-		"pictures": "503",
-		"games": "504",
-		"hdmovies": "505",
-		"movieclips": "506",
-		"other": "599"
+	porn: {
+		movies: '501',
+		moviesdvdr: '502',
+		pictures: '503',
+		games: '504',
+		hdmovies: '505',
+		movieclips: '506',
+		other: '599'
 	},
-	"other": {
-		"ebooks": "601",
-		"comics": "602",
-		"pictures": "603",
-		"covers": "604",
-		"physibles": "605",
-		"other": "699"
+	other: {
+		ebooks: '601',
+		comics: '602',
+		pictures: '603',
+		covers: '604',
+		physibles: '605',
+		other: '699'
 	}
 };
 
